c19f044e62ec7d01c189169a087ca46755af3434
Leonardo Balbino <leonardo.balbino2@fatec.sp.gov.br>
2025-02-28 11:52:24 -0300

    Primeiro commit

---
+++ js/calculadora.js
@@ -0,0 +1,8 @@
+function calcular(event) {
+    let primeiroValor = parseFloat.getDocumentById(primeiroValor)
+    let operador = document.getDocumentByID(operador)
+    let segundoValor = parseFloat.getDocumentById(segundoValor)
+
+    event.preventDefault(); // Previne o valor a ser passado na url mas por algum motivo não funciona, talvez pela lôgica ? 
+    //fazer switchcase para cada operador
+}
